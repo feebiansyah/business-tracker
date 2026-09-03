628d9c1b327772dc74b1ea1ef6095e0484785ab5
feebiansyah <anandafeebiansyah2@gmail.com>
2026-09-03 21:15:10 +0700

    config alloweddev origin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.101.7"],
+
   experimental: {
     serverActions: {
       bodySizeLimit: "11mb",
