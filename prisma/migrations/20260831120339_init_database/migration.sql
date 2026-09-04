561427eeebcc34bf9072251c7cc43d4ddda01994
feebiansyah <anandafeebiansyah2@gmail.com>
2026-09-04 18:03:00 +0700

    fix: make MySQL migration table casing portable

--- prisma/migrations/20260831120339_init_database/migration.sql
+++ prisma/migrations/20260831120339_init_database/migration.sql
@@ -1,17 +1,17 @@
 /*
   Warnings:
 
-  - Made the column `shopeeAccountId` on table `metaaccount` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `shopeeAccountId` on table `MetaAccount` required. This step will fail if there are existing NULL values in that column.
 
 */
 -- DropForeignKey
-ALTER TABLE `metaaccount` DROP FOREIGN KEY `MetaAccount_shopeeAccountId_fkey`;
+ALTER TABLE `MetaAccount` DROP FOREIGN KEY `MetaAccount_shopeeAccountId_fkey`;
 
 -- DropIndex
-DROP INDEX `MetaAccount_shopeeAccountId_fkey` ON `metaaccount`;
+DROP INDEX `MetaAccount_shopeeAccountId_fkey` ON `MetaAccount`;
 
 -- AlterTable
-ALTER TABLE `metaaccount` MODIFY `shopeeAccountId` INTEGER NOT NULL;
+ALTER TABLE `MetaAccount` MODIFY `shopeeAccountId` INTEGER NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE `MetaAccount` ADD CONSTRAINT `MetaAccount_shopeeAccountId_fkey` FOREIGN KEY (`shopeeAccountId`) REFERENCES `ShopeeAccount`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
